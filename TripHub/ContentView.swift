5c4b1365fe3b436ba67c86f078b7fba8a2ba1a9b
igrimzyi <1isaiahofficial1@gmail.com>
2022-08-16 12:04:34 -0700

    Initial Commit

---
+++ TripHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripHub
+//
+//  Created by Isaiah Tamayo on 8/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripHub/TripHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripHubApp.swift
+//  TripHub
+//
+//  Created by Isaiah Tamayo on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct TripHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
